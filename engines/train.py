6ca62e2bb33dfe233411abe540ea05d84417cf53
lishouxian <gzlishouxian@gmail.com>
2022-04-12 11:38:44 +0800

    Update train.py

--- engines/train.py
+++ engines/train.py
@@ -88,6 +88,61 @@ def train(configs, data_manager, logger):
             # 将预训练模型里面的pooler层的参数去掉
             variables = [var for var in variables if 'pooler' not in var.name]
             gradients = tape.gradient(loss, variables)
+
+            if configs.use_gan:
+                if configs.gan_method == 'fgm':
+                    # 使用FGM的对抗办法
+                    epsilon = 1.0
+                    embedding = variables[0]
+                    embedding_gradients = gradients[0]
+                    embedding_gradients = tf.zeros_like(embedding) + embedding_gradients
+                    delta = epsilon * embedding_gradients / tf.norm(embedding_gradients, ord=2)
+
+                    accum_vars = [tf.Variable(tf.zeros_like(grad), trainable=False) for grad in gradients]
+                    gradients = [accum_vars[i].assign_add(grad) for i, grad in enumerate(gradients)]
+                    variables[0].assign_add(delta)
+
+                    with tf.GradientTape() as gan_tape:
+                        logits, log_likelihood, _ = ner_model(inputs=model_inputs, inputs_length=inputs_length,
+                                                 targets=y_train_batch, training=1)
+                        loss = -tf.reduce_mean(log_likelihood)
+                    gan_gradients = gan_tape.gradient(loss, variables)
+                    gradients = [gradients[i].assign_add(grad) for i, grad in enumerate(gan_gradients)]
+                    variables[0].assign_sub(delta)
+
+                elif configs.gan_method == 'pgd':
+                    # 使用PGD的对抗办法
+                    K = 3
+                    alpha = 0.3
+                    epsilon = 1
+                    origin_embedding = tf.Variable(variables[0])
+                    accum_vars = [tf.Variable(tf.zeros_like(grad), trainable=False) for grad in gradients]
+                    origin_gradients = [accum_vars[i].assign_add(grad) for i, grad in enumerate(gradients)]
+
+                    for t in range(K):
+                        embedding = variables[0]
+                        embedding_gradients = gradients[0]
+                        embedding_gradients = tf.zeros_like(embedding) + embedding_gradients
+                        delta = alpha * embedding_gradients / tf.norm(embedding_gradients, ord=2)
+                        variables[0].assign_add(delta)
+
+                        r = variables[0] - origin_embedding
+                        if tf.norm(r, ord=2) > epsilon:
+                            r = epsilon * r / tf.norm(r, ord=2)
+                        variables[0].assign(origin_embedding + tf.Variable(r))
+
+                        if t != K - 1:
+                            gradients = [tf.Variable(tf.zeros_like(grad), trainable=False) for grad in gradients]
+                        else:
+                            gradients = origin_gradients
+                        with tf.GradientTape() as gan_tape:
+                            logits, log_likelihood, _ = ner_model(inputs=model_inputs, inputs_length=inputs_length,
+                                                                  targets=y_train_batch, training=1)
+                            loss = -tf.reduce_mean(log_likelihood)
+                        gan_gradients = gan_tape.gradient(loss, variables)
+                        gradients = [gradients[i].assign_add(grad) for i, grad in enumerate(gan_gradients)]
+                    variables[0].assign(origin_embedding)
+
             # 反向传播，自动微分计算
             optimizer.apply_gradients(zip(gradients, variables))
             if step % configs.print_per_batch == 0 and step != 0:
